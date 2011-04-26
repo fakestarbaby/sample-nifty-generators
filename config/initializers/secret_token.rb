a0048e7d1608deeb0f1bbcb0868b81fd66d9fe1c
fakestarbaby <fakestarbaby@gmail.com>
2011-04-26 22:26:50 +0900

    初回コミット。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleNiftyGenerators::Application.config.secret_token = 'c94a95ef4f7aa60c2cc0dea61bb0ed2eb1d60cd65e569371423b9fa9a662bf462002eb7e971b136553ad32d9c9dae4ce47e40abecccfa1c62fe2451fd7876713'
